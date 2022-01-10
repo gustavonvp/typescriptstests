8dc08d0f47a43cf028d4c9f40f968b2bcd39560f
Gustavonvp <gustavonvp@gmail.com>
2022-01-10 02:53:28 -0300

    destructuring the interface function with lazy load

--- functions.ts
+++ functions.ts
@@ -25,3 +25,19 @@ const throwError = (message: string): string => {
 
     return message;
 };
+
+
+const todaysWeather = {
+    date: new Date(),
+    weather: 'sunny'
+};
+
+const logWeather = 
+(
+    {date, weather} : { date: Date, weather: string}
+): void => {
+    console.log(date);
+    console.log(weather);
+};
+
+logWeather(todaysWeather);
