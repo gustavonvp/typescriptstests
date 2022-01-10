401082652dd6a93731d5b25fa834f67e9352c688
Gustavonvp <gustavonvp@gmail.com>
2022-01-10 01:45:52 -0300

    2 types in a lazy initialization

--- variable.ts
+++ variable.ts
@@ -51,3 +51,14 @@ for(let i = 0; i < word.length; i++) {
         foundWord = true; //lazy initialization
     }
 }
+
+
+// 3) Variables whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+
+for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] > 0) {
+        numberAboveZero = numbers[i];
+    }
+}
