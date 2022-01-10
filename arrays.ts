dc3a20209fd52a3ee8b9f73b296dfed8c78d6395
Gustavonvp <gustavonvp@gmail.com>
2022-01-10 13:19:10 -0300

    tuples and type inference

--- arrays.ts
+++ arrays.ts
@@ -4,12 +4,17 @@ const dates = [new Date(), new Date()];
 
 const carsByMake: string[][] = [];
 
-// Help with inference when extracting values
-
-//prevent incompatible values
-carMakers.push(); //can push string
-
 //help with'map'
 carMakers.map((car: string): string =>{
     return car.toUpperCase();
 });
+
+//Flexible types
+const importantDates = [new Date(), '2030-10-10'];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
+
+// Help with inference when extracting values
+//prevent incompatible values
+carMakers.push(); //can push string
+importantDates.push(); //can push string or date
---
+++ tuples.ts
@@ -0,0 +1,18 @@
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+};
+
+type Drink = [string, boolean, number];
+
+const pepsi = ['brown', true, 40];
+
+// passing type
+const pepsiTyped: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
+
+//Tuples elements are acepted with this type
+// pepsi[0]= 40;
+// pepsi[2] = true;
